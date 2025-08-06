b017a329f394152a57cf79c583d9e447bc7ce551
sua00 <suasua0105@gmail.com>
2025-08-07 08:58:12 +0900

    docs : ioUtils dependencies 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("commons-io:commons-io:2.11.0")
 }
