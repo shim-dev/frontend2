8851afd7fff0230bc2724be82e39e131fb89acd4
sua00 <suasua0105@gmail.com>
2025-08-10 16:21:39 +0900

    feat : 게시물 작성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation ("androidx.appcompat:appcompat:1.7.0")
+
 }
